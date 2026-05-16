17fd67d8a5e74dcaa78f8a8fdaf572ef4b0d6719
HARSHIDS-4 <gharshi089@gmail.com>
2026-05-17 03:39:02 +0530

    deploy

--- frontend.py
+++ frontend.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
 from datetime import datetime
-API_URL="http://127.0.0.1:8000/predict"
+API_URL="https://autoworthai.onrender.com/predict"
 
 st.title("Car Price Prediction")
 st.markdown("Enter the details to predict the car price: ")
