74dbb21a1a412bffbaee43dbf7d540a8af51033b
Harshi Gupta <gharshi089@gmail.com>
2026-05-20 05:14:42 +0530

    Update API_URL to production endpoint

--- frontend.py
+++ frontend.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
 from datetime import datetime
-API_URL="http://127.0.0.1:8000/predict"
+API_URL="https://autoworth-ai-ml-based-car-price-estimator.onrender.com"
 
 
 
